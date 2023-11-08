4d3fb995819dbc0af69ddc1d2c1fbbbe2b7196d8
Muhammad Farabie <farabie024@gmail.com>
2023-11-08 09:33:25 +0700

    Menambahkan class Router dan menambah url map

---
+++ app/APP/router.php
@@ -0,0 +1,22 @@
+<?php 
+
+namespace BieProject\Belajar\PHP\MVC\APP;
+
+class Router {
+    private static array $routes = [];
+
+    public static function add(string $method, string $path, string $controller, string $function): void {
+        self::$routes[] = [
+            'method' => $method,
+            'path' => $path,
+            'controller' => $controller,
+            'function'=> $function,
+        ];
+    }
+
+    public static function run():void {
+
+    }
+}
+
+?>
