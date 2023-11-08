6bde8099c4e55d8bad0058f4559934136d6c3ab4
Muhammad Farabie <farabie024@gmail.com>
2023-11-08 09:15:27 +0700

    Router Sederhana

--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
-<?php 
+<?php
 
-echo "Hello PHP MVC";
+$path = '/index';
 
+if(isset($_SERVER['PATH_INFO'])) {
+    $path = $_SERVER['PATH_INFO'];
+}
+
+
+require_once __DIR__ . '/../app/View/' . $path . '.php';
 ?>
