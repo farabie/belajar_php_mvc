7d386e5e5bc0f31ed4d8ee2327580d447666f3af
Muhammad Farabie <farabie024@gmail.com>
2023-11-08 13:28:39 +0700

    Model

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -4,7 +4,16 @@ namespace  BieProject\Belajar\PHP\MVC\Controller;
 
 
 class HomeController {
+
     function index(): void {
+        $title = "Belajar PHP MVC";
+        $content = "Semangat belajar PHP MVC nya";
+
+        $model = [
+            "title" => $title,
+            "content" => $content,
+        ];
+
         echo "HomeControleer.index()";
     }
     function hello(): void {
@@ -18,6 +27,21 @@ class HomeController {
     function author(): void
     {
         echo "Hello Author: Muhammad Farabie";
+    }
+
+    function login(): void {
+        $request = [
+            "username" => $_POST['username'],
+            "password" => $_POST["password"],
+        ];
+
+        $user = [];
+
+        $response = [
+            "message" => "Anda berhasil login"
+        ];
+
+        //Kirimkan response tadi ke view
     }
 }
 
