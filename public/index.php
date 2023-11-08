b2045d33a562bcb3210c7ac8d69f1914d5494360
Muhammad Farabie <farabie024@gmail.com>
2023-11-08 14:03:39 +0700

    Middleware

--- public/index.php
+++ public/index.php
@@ -4,13 +4,14 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use BieProject\Belajar\PHP\MVC\APP\Router;
 use BieProject\Belajar\PHP\MVC\Controller\HomeController;
 use BieProject\Belajar\PHP\MVC\Controller\ProductController;
+use BieProject\Belajar\PHP\MVC\Middleware\AuthMiddleware;
 
 
 Router::add('GET', '/products/([0-9a-zA-Z]*)/categories/([0-9a-zA-Z]*)', ProductController::class, 'categories');
 
 Router::add('GET', '/', HomeController::class, 'index');
-Router::add('GET', '/hello', HomeController::class, 'hello');
-Router::add('GET', '/world', HomeController::class, 'world');
+Router::add('GET', '/hello', HomeController::class, 'hello', [AuthMiddleware::class]);
+Router::add('GET', '/world', HomeController::class, 'world', [AuthMiddleware::class]);
 Router::add('GET', '/author', HomeController::class, 'author');
 
 Router::run();
